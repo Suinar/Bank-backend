020009003ff7a5b338e1ad88ef83ac9cc77ccdad
Mykola <suinar.ipad@gmail.com>
2026-03-18 12:37:33 +0200

    feature/BB-add-base-errors
    Added 3 errors: BadReuestm, NotFound ,InternalServerError.

---
+++ bankBackend/internal/core/errors.go
@@ -0,0 +1,9 @@
+﻿package core
+
+import "errors"
+
+var (
+	NotFound            = errors.New("not found")
+	BadRequest          = errors.New("bad request")
+	InternalServerError = errors.New("internal server error")
+)
